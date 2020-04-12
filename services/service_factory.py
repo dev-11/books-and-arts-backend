688e293ac276db6b399cfeb5c09081f4de0a7af2
Otto <otto@masterbranch.io>
2020-04-12 17:09:45 +0100

    build error fix

--- services/service_factory.py
+++ services/service_factory.py
@@ -5,7 +5,7 @@ import config
 def get_all_services():
     return [ws.BooksOfTheMonthService(config.books_of_the_month_url),
             ws.ComingSoonService(config.coming_soon_url),
-            ws.NewBooksService()]
+            ws.NewBooksService(config.new_books_url)]
 
 
 def get_enabled_services():
