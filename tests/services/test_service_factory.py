0edde035e96c588ec5291fc7db576daa8446364a
Otto <otto@masterbranch.io>
2020-04-12 10:03:19 +0100

    typo fix

--- tests/services/test_service_factory.py
+++ tests/services/test_service_factory.py
@@ -8,7 +8,7 @@ class ServiceFactoryTests(unittest.TestCase):
         all_services = service_factory.get_all_services()
         self.assertEqual(3, len(all_services))
 
-    def test_get_enabled_services_return_only_enabled_services(self):
+    def test_get_enabled_services_returns_only_enabled_services(self):
         enabled_services = service_factory.get_enabled_services()
         for service in enabled_services:
             self.assertIn(service.get_service_name(), config.enabled_services)
