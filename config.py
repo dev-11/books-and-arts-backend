9cf49ace78215a509f6427f2eceb5e8f3cd1cdb0
Otto <otto@masterbranch.io>
2020-04-12 17:03:22 +0100

    adding new books service

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 books_of_the_month_url = 'https://www.waterstones.com/campaign/books-of-the-month'
 coming_soon_url = 'https://www.waterstones.com/campaign/coming-soon'
+new_books_url = 'https://www.waterstones.com/campaign/new-books'
 
 enabled_services = [
-    'coming_soon'
+    'books_of_the_month',
+    'coming_soon',
+    'new_book'
 ]
--- services/waterstones/new_books_service.py
+++ services/waterstones/new_books_service.py
@@ -1,10 +1,46 @@
+import requests
+import bs4
 from services import ServiceStrategy
 
 
 class NewBooksService(ServiceStrategy):
+    def __init__(self, url):
+        self._url = url
+
     def get_service_name(self):
-        return "new_books_service"
+        return "new_books"
+
+    @staticmethod
+    def get_book_details(divs):
+        section = divs.find('h2').text.strip()
+        book_previews = divs.find_all(class_='book-preview')
+        books = []
+        for _ in book_previews:
+            title = _.find(class_='title-wrap').text.strip()
+            authors = _.find(class_='author-wrap').text.strip()
+            price = _.find(class_='price').text.strip()
+            frmt = _.find(class_='format').text.strip()
+            books.append({
+                'title': title.encode("utf-8"),
+                'authors': authors.encode("utf-8"),
+                'price': price.encode("utf-8"),
+                'format': frmt.encode("utf-8")
+            })
+
+        return {
+            'section': section.encode("utf-8"),
+            'books': books
+        }
 
     def get_data(self):
-        pass
+        page = requests.get(self._url)
 
+        soup = bs4.BeautifulSoup(page.text, 'html.parser')
+
+        lst = soup.find(class_='row home-row')
+        lst2 = lst.find_all(class_='span12')
+
+        bom = lst2[1:]
+        bom.pop(1)  # removing 'special' section
+
+        return [self.get_book_details(pair) for pair in bom]
