bb18e5bf3a26721690e34979633577dfe3a8e197
Otto <otto@masterbranch.io>
2020-04-16 21:38:40 +0100

    adding full name to json response

--- entry_point/app.py
+++ entry_point/app.py
@@ -5,9 +5,10 @@ import json
 
 def lambda_handler(event, context):
 
-    data = [{'family':  service.get_service_family_name(),
-             'service': service.get_service_name(),
-             'data':    service.get_data()}
+    data = [{'family':    service.get_service_family_name(),
+             'service':   service.get_service_name(),
+             'full_name': service.get_service_full_name(),
+             'data':      service.get_data()}
             for service in get_enabled_services()]
 
     return {
--- services/service_strategy.py
+++ services/service_strategy.py
@@ -13,3 +13,6 @@ class ServiceStrategy(ABC):
     @abstractmethod
     def get_data(self):
         pass
+
+    def get_service_full_name(self):
+        return f'{self.get_service_family_name()}.{self.get_service_name()}'
