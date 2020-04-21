f61da400db3f5114972e176dfa2ad7b4d2ae366c
Otto <otto@masterbranch.io>
2020-04-21 15:17:19 +0100

    parsing bool header

--- entry_point/app.py
+++ entry_point/app.py
@@ -5,16 +5,15 @@ import config as c
 
 
 def lambda_handler(event, context):
-
     headers = event['params']['header']
-    is_hard_get = headers[c.hard_load_header_key] \
+    is_hard_get = parse_bool(headers[c.hard_load_header_key]) \
         if c.hard_load_header_key in headers \
         else False
 
-    data = [{'family':    service.get_service_family_name(),
-             'service':   service.get_service_name(),
+    data = [{'family': service.get_service_family_name(),
+             'service': service.get_service_name(),
              'full_name': service.get_service_full_name(),
-             'data':      service.get_data(is_hard_get)}
+             'data': service.get_data(is_hard_get)}
             for service in get_enabled_services()]
 
     return {
@@ -24,3 +23,7 @@ def lambda_handler(event, context):
             'data': data
         }
     }
+
+
+def parse_bool(v):
+    return str(v).lower() in ("yes", "true", "t", "1")
