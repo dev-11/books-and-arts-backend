f516ba8f8e64eaebf8b472fa20c853f653a5e13d
Otto <otto@masterbranch.io>
2020-05-17 00:45:17 +0100

    Update secret_repository.py

--- repositories/secret_repository.py
+++ repositories/secret_repository.py
@@ -1,4 +1,3 @@
-import boto3
 import os
 
 
@@ -8,11 +7,8 @@ class SecretRepository:
 
 
 class SSMRepository(SecretRepository):
-    def __init__(self):
-        self._ssm = boto3.resource('ssm')
-
     def get_parameter(self, parameter_name):
-        return self._ssm.get_parameter(parameter_name)
+        return os.environ[parameter_name]
 
 
 class LocalSecretRepository(SecretRepository):
