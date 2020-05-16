004bee5f317b4e75fc533233e9d64bb1581fcac3
Otto <otto@masterbranch.io>
2020-05-16 17:34:03 +0100

    factory fix

--- services/service_factory.py
+++ services/service_factory.py
@@ -16,7 +16,7 @@ class ServiceFactory:
             key = secrets.good_reads_api_key
         except AttributeError:
             key = ''
-        self._rating_service = ws.MergingService(key)
+        self._merging_service = ws.MergingService(ws.RatingService(key))
 
     def get_all_services(self):
         return [self.get_waterstones_books_of_the_month_service(),
@@ -35,15 +35,15 @@ class ServiceFactory:
 
     def get_waterstones_books_of_the_month_service(self):
         scraping_service = ws.BooksOfTheMonthScrapingService(config.books_of_the_month_url)
-        return ws.BooksOfTheMonthService(scraping_service, self._cache, self._rating_service)
+        return ws.BooksOfTheMonthService(scraping_service, self._cache, self._merging_service)
 
     def get_waterstones_coming_soon_service(self):
         scraper = ws.ComingSoonScrapingService(config.coming_soon_url)
-        return ws.ComingSoonService(scraper, self._cache, self._rating_service)
+        return ws.ComingSoonService(scraper, self._cache, self._merging_service)
 
     def get_waterstones_new_books_service(self):
         scraper = ws.NewBooksScrapingService(config.new_books_url)
-        return ws.NewBooksService(scraper, self._cache, self._rating_service)
+        return ws.NewBooksService(scraper, self._cache, self._merging_service)
 
 
 def get_enabled_services():
