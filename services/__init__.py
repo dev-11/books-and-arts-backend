4683d1af3a00f7d6c4b15641befd0e5a1edfe924
Otto <otto@masterbranch.io>
2020-05-17 22:19:23 +0100

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,4 @@
-"""
-Services to collect all the data for the lambda.
-"""
+"""Services to collect all the data for the lambda."""
 from .service_strategy import ServiceStrategy, ScrapingServiceBase
 from .storage_service import StorageService
 from .cache_service import CacheService
