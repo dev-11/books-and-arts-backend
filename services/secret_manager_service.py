f11eec406d5fae7cbf96f47227b71fe306b2d14c
Otto <otto@masterbranch.io>
2020-05-17 10:51:51 +0100

    test fix

--- services/secret_manager_service.py
+++ services/secret_manager_service.py
@@ -1,9 +1,9 @@
-from repositories.secret_repository import SecretRepository
+from repositories.environment_repository import EnvironmentRepository
 
 
 class SecretManagerService:
-    def __init__(self, secret_repository: SecretRepository):
-        self._secret_repository = secret_repository
+    def __init__(self, repository: EnvironmentRepository):
+        self._repository = repository
 
     def get_secret(self, secret_name):
-        return self._secret_repository.get_parameter(secret_name)
+        return self._repository.get_parameter(secret_name)
