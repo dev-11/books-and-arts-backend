44dfbf94b5088f626974a5221a627956c227213f
Otto <otto@masterbranch.io>
2020-05-17 13:18:31 +0100

    test fix

--- tests/test_environment/mocks.py
+++ tests/test_environment/mocks.py
@@ -7,5 +7,7 @@ def get_mocked_s3repo_returns_empty_body():
     s3r.get_body = Mock(name='get_body')
     s3r.get_body.return_value = '{}'
     s3r.get_metadata = Mock(name='get_metadata')
-    s3r.get_body.return_value = ''
+    s3r.get_metadata.return_value = ''
+    s3r.has_key = Mock(name='has_key')
+    s3r.has_key.return_value = False
     return s3r
