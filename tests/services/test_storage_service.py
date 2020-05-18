3abc7e7992b007c33345cc52f0db4b948beb9a57
Otto <otto@masterbranch.io>
2020-05-18 19:02:50 +0100

    testing if condition

--- tests/services/test_storage_service.py
+++ tests/services/test_storage_service.py
@@ -10,7 +10,7 @@ class StorageServiceTests(unittest.TestCase):
         result = ss.get('test-key')
         self.assertEqual({}, result)
 
-    def test_get_expiry_date_returns_default_value_for_missing_metadata(self):
+    def test_get_expiry_date_returns_default_value_for_missing_key(self):
         ss = StorageService(mocks.get_mocked_s3repo_returns_empty_body())
         result = ss.get_cache_update_date('test-key')
         self.assertEqual(dt(2000, 1, 1, 0, 0, 0), result)
@@ -25,3 +25,7 @@ class StorageServiceTests(unittest.TestCase):
         result = ss.get_cache_update_date('test-key')
         self.assertEqual(dt.fromisoformat('2020-03-20T14:28:23.382748'), result)
 
+    def test_get_cache_update_date_returns_default_value_for_missing_metadata(self):
+        ss = StorageService(mocks.get_mocked_s3repo_has_key_but_no_metadata())
+        result = ss.get_cache_update_date('test-key')
+        self.assertEqual(dt(2000, 1, 1, 0, 0, 0), result)
--- tests/test_environment/mocks.py
+++ tests/test_environment/mocks.py
@@ -16,6 +16,18 @@ def get_mocked_s3repo_returns_empty_body():
     s3r.save_or_update_file.return_value = False
     return s3r
 
+def get_mocked_s3repo_has_key_but_no_metadata():
+    s3r = S3Repository('test- bucket')
+    s3r.get_body = Mock(name='get_body')
+    s3r.get_body.return_value = '{}'
+    s3r.get_metadata = Mock(name='get_metadata')
+    s3r.get_metadata.return_value = ''
+    s3r.has_key = Mock(name='has_key')
+    s3r.has_key.return_value = True
+    s3r.save_or_update_file = Mock(name='save_or_update_file')
+    s3r.save_or_update_file.return_value = False
+    return s3r
+
 def get_mocked_s3repo_returns_cache_update_date():
     s3r = S3Repository('test- bucket')
     s3r.get_metadata = Mock(name='get_metadata')
