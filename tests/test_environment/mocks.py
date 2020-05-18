c8f59eb6bd85b5d81887a15a5434ee306d326cb0
Otto <otto@masterbranch.io>
2020-05-18 19:05:22 +0100

    mock fix

--- tests/test_environment/mocks.py
+++ tests/test_environment/mocks.py
@@ -21,7 +21,7 @@ def get_mocked_s3repo_has_key_but_no_metadata():
     s3r.get_body = Mock(name='get_body')
     s3r.get_body.return_value = '{}'
     s3r.get_metadata = Mock(name='get_metadata')
-    s3r.get_metadata.return_value = ''
+    s3r.get_metadata.return_value = {}
     s3r.has_key = Mock(name='has_key')
     s3r.has_key.return_value = True
     s3r.save_or_update_file = Mock(name='save_or_update_file')
